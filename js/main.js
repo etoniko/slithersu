206a9b14fc241867077519789cb1bbe7d7270788
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-07-31 17:24:51 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,4 +0,0 @@
-import { Game } from "./core/Game.js";
-
-const core = new Game();
-window.CORE = core;
--- js/utils/array.js
+++
@@ -1,4 +0,0 @@
-export function removeFromArray(arr, item) {
-  const i = arr.indexOf(item);
-  return i !== -1 && arr.splice(i, 1);
-}
