29c3f18045370f0a0a2f82aef1667c6262c2b4e3
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-08-15 14:31:56 +0300

    Update servers.js

--- js/config/servers.js
+++ js/config/servers.js
@@ -1,5 +1,5 @@
 /** Единственный игровой сервер (WSS через прокси sixz.ru). */
-export const SERVER_HOST = "sixz.ru/slither";
+export const SERVER_HOST = "sixz.ru:6009";
 export const SERVER_WS_URL = `wss://${SERVER_HOST}`;
 
 export const servers = {
